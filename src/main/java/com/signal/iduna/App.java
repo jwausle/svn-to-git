50bdce74d3fb0812a48ee8e012d2ebf40608add7
Jan Winter <jan.winter@itemis.de>
2018-07-06 15:27:20 +0200

    Migrate to maven project.

---
+++ src/main/java/com/signal/iduna/App.java
@@ -0,0 +1,13 @@
+package com.signal.iduna;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Signal Iduna svn to git workshop!" );
+    }
+}
---
+++ src/test/java/com/signal/iduna/AppTest.java
@@ -0,0 +1,16 @@
+package com.signal.iduna;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest
+{
+    @Test
+    public void testApp()
+    {
+        Assert.assertTrue( true );//
+    }
+}
